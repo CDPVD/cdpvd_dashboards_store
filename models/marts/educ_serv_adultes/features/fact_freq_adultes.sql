d58ad48110b27dd837cac89da7dff65241be4c4c
alluardj <alluardj@csvdc.qc.ca>
2024-11-25 15:34:16 -0500

    feat: fact_freq_adultes sqlfmt

--- models/marts/educ_serv_adultes/features/fact_freq_adultes.sql
+++ models/marts/educ_serv_adultes/features/fact_freq_adultes.sql
@@ -24,61 +24,78 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
     config(
         post_hook=[
             create_clustered_index(
-                "{{ this }}", ["fiche", "annee", "freq"]
+                "{{ this }}", ["code_perm", "fiche", "annee", "freq", "population"]
             )
         ]
     )
 }}
-     SELECT pop.code_perm
-          , pop.fiche
-          , freq.client
-          , pop.population
-          , pop.annee
-          , pop.freq
-          , floor(datediff(day, convert(date, el.date_naissance, 112) , cast(concat(pop.annee, '0630') AS date)) / 365.25) AS age_30_juin
-            /* format 112 = yyyymmdd */
-          , floor(datediff(day, convert(date, el.date_naissance, 112) , cast(concat(pop.annee, '0930') AS date)) / 365.25) AS age_30_septembre
-            /* format 112 = yyyymmdd */
-          , freq.org
-          , freq.eco_cen
-          , freq.bat
-          , freq.org_hor
-          , org.descr AS descr_org_hor
-          , freq.date_deb
-          , freq.date_fin_sifca
-          , freq.prog
-          , prog.prog_meq
-          , prog.descr_prog
-          , prog.type_diplome
-          , freq.type_activ
-          , freq.type_parcours
-          , wlt.cf_descr AS descr_type_parcours
-          , freq.service_enseign
-          , wld.cf_descr AS descr_service_enseign
-          , freq.motif_depart
-          , mot.descr AS descr_motif_dep
-          , freq.raison_depart
-          , wl.cf_descr desc_raison_depart
-       FROM {{ ref("stg_populations_adultes") }} AS pop
-       JOIN {{ ref("dim_eleve_adultes") }} AS el
-     ON el.code_perm = pop.code_perm
-       JOIN {{ ref("i_e_freq_adultes") }} AS freq
-     ON freq.fiche = pop.fiche
-        AND freq.annee = pop.annee
-        AND freq.freq = pop.freq
-  LEFT JOIN {{ ref("i_t_prog_adultes") }} prog
-         ON prog.prog = freq.prog
-  LEFT JOIN {{ ref("i_t_wl_descr_adultes") }} wld
-         ON wld.code = freq.service_enseign
-        AND wld.nom_table = 'X_ServiceEnseign'
-  LEFT JOIN {{ ref("i_t_motif_adultes") }} mot
-         ON mot.motif = freq.motif_depart
-  LEFT JOIN {{ ref("i_t_wl_descr_adultes") }} wl
-         ON wl.code = freq.raison_depart
-        AND wl.nom_table = 'X_RaisonDepart'
-  LEFT JOIN {{ ref("i_t_wl_descr_adultes") }} wlt
-         ON wlt.code = freq.type_parcours
-        AND wlt.nom_table = 'X_TypeParcours'
-  LEFT JOIN {{ ref("i_e_o_orghor") }} org
-         ON org.eco_cen = freq.eco_cen
-        AND org.org_hor = freq.org_hor
+select
+    pop.code_perm,
+    pop.fiche,
+    freq.client,
+    pop.population,
+    pop.annee,
+    pop.freq,
+    floor(
+        datediff(
+            day,
+            convert(date, el.date_naissance, 112),
+            cast(concat(pop.annee, '0630') as date)
+        )
+        / 365.25
+    ) as age_30_juin,
+    /* format 112 = yyyymmdd */
+    floor(
+        datediff(
+            day,
+            convert(date, el.date_naissance, 112),
+            cast(concat(pop.annee, '0930') as date)
+        )
+        / 365.25
+    ) as age_30_septembre,
+    /* format 112 = yyyymmdd */
+    freq.org,
+    freq.eco_cen,
+    freq.bat,
+    freq.org_hor,
+    org.descr as descr_org_hor,
+    freq.date_deb,
+    freq.date_fin_sifca,
+    freq.prog,
+    prog.prog_meq,
+    prog.descr_prog,
+    prog.type_diplome,
+    freq.type_activ,
+    freq.type_parcours,
+    wlt.cf_descr as descr_type_parcours,
+    freq.service_enseign,
+    wld.cf_descr as descr_service_enseign,
+    freq.motif_depart,
+    mot.descr as descr_motif_dep,
+    freq.raison_depart,
+    wl.cf_descr desc_raison_depart
+from {{ ref("stg_populations_adultes") }} as pop
+join {{ ref("dim_eleve_adultes") }} as el on el.code_perm = pop.code_perm
+join
+    {{ ref("i_e_freq_adultes") }} as freq
+    on freq.fiche = pop.fiche
+    and freq.annee = pop.annee
+    and freq.freq = pop.freq
+left join {{ ref("i_t_prog_adultes") }} prog on prog.prog = freq.prog
+left join
+    {{ ref("i_t_wl_descr_adultes") }} wld
+    on wld.code = freq.service_enseign
+    and wld.nom_table = 'X_ServiceEnseign'
+left join {{ ref("i_t_motif_adultes") }} mot on mot.motif = freq.motif_depart
+left join
+    {{ ref("i_t_wl_descr_adultes") }} wl
+    on wl.code = freq.raison_depart
+    and wl.nom_table = 'X_RaisonDepart'
+left join
+    {{ ref("i_t_wl_descr_adultes") }} wlt
+    on wlt.code = freq.type_parcours
+    and wlt.nom_table = 'X_TypeParcours'
+left join
+    {{ ref("i_e_o_orghor") }} org
+    on org.eco_cen = freq.eco_cen
+    and org.org_hor = freq.org_hor
