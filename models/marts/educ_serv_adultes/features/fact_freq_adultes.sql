33ce3f27b1d0c3ddbce37924880c8622c4d07ac5
alluardj <alluardj@csvdc.qc.ca>
2024-11-26 08:28:29 -0500

    chore: new try to license

--- models/marts/educ_serv_adultes/features/fact_freq_adultes.sql
+++ models/marts/educ_serv_adultes/features/fact_freq_adultes.sql
@@ -1,24 +1,19 @@
 {#
 CDPVD Dashboards store
 Copyright (C) 2024 CDPVD.
- 
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
 published by the Free Software Foundation, either version 3 of the
 License, or any later version.
- 
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Affero General Public License for more details.
- 
+
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#}
-{#
-  Map each school to it's friendly name.
-
-  Feel free to override me to get your own custom litle mapping.
 #}
 {{
     config(
