76f34d18f8cd765ca8b931501f2a0f9554499958
jalluard <159054441+jalluard@users.noreply.github.com>
2024-12-02 08:24:19 -0500

    feat(educ_serv_adultes) : identifier les frequentations en FP/FGA (#4)
    * chore : update les depondance du repo core
    
    * doc: Update README.md
    
    * feat: add the descr_org_hor + interface
    
    * feat: add age_30_sept / june in fact_freq_adultes
    
    * feat: add type_parcours field to fact_freq and adjust interface field names
    
    * feat: interfaces jade adultes
    
    * feat: dim_eleves_adultes
    
    * feat: fact_freq_adultes sqlfmt
    
    * chore: modify license in scripts
    
    * chore: new try to license
    
    ---------
    
    Co-authored-by: sadqim <sadqim@csvdc.qc.ca>
    Co-authored-by: Frederyk Busque <147734849+busquef@users.noreply.github.com>
    Co-authored-by: alluardj <alluardj@csvdc.qc.ca>
    Co-authored-by: Adama Fall <fallada@cs-soreltracy.qc.ca>

---
+++ models/marts/educ_serv_adultes/features/fact_freq_adultes.sql
@@ -0,0 +1,96 @@
+{#
+CDPVD Dashboards store
+Copyright (C) 2024 CDPVD.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#}
+{{
+    config(
+        post_hook=[
+            create_clustered_index(
+                "{{ this }}", ["code_perm", "fiche", "annee", "freq", "population"]
+            )
+        ]
+    )
+}}
+select
+    pop.code_perm,
+    pop.fiche,
+    freq.client,
+    pop.population,
+    pop.annee,
+    pop.freq,
+    floor(
+        datediff(
+            day,
+            convert(date, el.date_naissance, 112),
+            cast(concat(pop.annee, '0630') as date)
+        )
+        / 365.25
+    ) as age_30_juin,
+    /* format 112 = yyyymmdd */
+    floor(
+        datediff(
+            day,
+            convert(date, el.date_naissance, 112),
+            cast(concat(pop.annee, '0930') as date)
+        )
+        / 365.25
+    ) as age_30_septembre,
+    /* format 112 = yyyymmdd */
+    freq.org,
+    freq.eco_cen,
+    freq.bat,
+    freq.org_hor,
+    org.descr as descr_org_hor,
+    freq.date_deb,
+    freq.date_fin_sifca,
+    freq.prog,
+    prog.prog_meq,
+    prog.descr_prog,
+    prog.type_diplome,
+    freq.type_activ,
+    freq.type_parcours,
+    wlt.cf_descr as descr_type_parcours,
+    freq.service_enseign,
+    wld.cf_descr as descr_service_enseign,
+    freq.motif_depart,
+    mot.descr as descr_motif_dep,
+    freq.raison_depart,
+    wl.cf_descr desc_raison_depart
+from {{ ref("stg_populations_adultes") }} as pop
+join {{ ref("dim_eleve_adultes") }} as el on el.code_perm = pop.code_perm
+join
+    {{ ref("i_e_freq_adultes") }} as freq
+    on freq.fiche = pop.fiche
+    and freq.annee = pop.annee
+    and freq.freq = pop.freq
+left join {{ ref("i_t_prog_adultes") }} prog on prog.prog = freq.prog
+left join
+    {{ ref("i_t_wl_descr_adultes") }} wld
+    on wld.code = freq.service_enseign
+    and wld.nom_table = 'X_ServiceEnseign'
+left join {{ ref("i_t_motif_adultes") }} mot on mot.motif = freq.motif_depart
+left join
+    {{ ref("i_t_wl_descr_adultes") }} wl
+    on wl.code = freq.raison_depart
+    and wl.nom_table = 'X_RaisonDepart'
+left join
+    {{ ref("i_t_wl_descr_adultes") }} wlt
+    on wlt.code = freq.type_parcours
+    and wlt.nom_table = 'X_TypeParcours'
+left join
+    {{ ref("i_e_o_orghor") }} org
+    on org.eco_cen = freq.eco_cen
+    and org.org_hor = freq.org_hor
