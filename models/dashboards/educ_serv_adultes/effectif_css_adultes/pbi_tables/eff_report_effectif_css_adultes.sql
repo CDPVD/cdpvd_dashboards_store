ba581c385c53b6d54de54965145bb5b1b98a1c42
Adama Fall <fallada@cs-soreltracy.qc.ca>
2024-12-09 10:16:14 -0500

    Table power bi

---
+++ models/dashboards/educ_serv_adultes/effectif_css_adultes/pbi_tables/eff_report_effectif_css_adultes.sql
@@ -0,0 +1,113 @@
+{#
+Dashboards Store - Helping students, one dashboard at a time.
+Copyright (C) 2023  Sciance Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#}
+{{ config(alias="report_effectif_css_adultes",tags=["effectif_css_adultes","populations_adultes","educ_serv_adultes"],schema="dashboard_effectif_css_adultes",
+post_hook=[stamp_model("dashboard_effectif_css_adultes")]) }}
+
+    
+with
+    agg as (
+        select
+            client,
+            population,
+            annee,
+            freq,
+            age_30_juin,
+            age_30_septembre,
+            org,
+            eco_cen,
+            bat,
+            org_hor,
+            descr_org_hor,
+            date_deb,
+            date_fin_sifca,
+            prog,
+            prog_meq,
+            descr_prog,
+            type_diplome,
+            type_activ,
+            type_parcours,
+            descr_type_parcours,
+            service_enseign,
+            descr_service_enseign,
+            motif_depart,
+            descr_motif_dep,
+            raison_depart,
+            desc_raison_depart,
+            count(code_perm) as total_ele
+        from {{ ref("fact_freq_adultes") }}
+        group by
+            client,
+            population,
+            annee,
+            freq,
+            age_30_juin,
+            age_30_septembre,
+            org,
+            eco_cen,
+            bat,
+            org_hor,
+            descr_org_hor,
+            date_deb,
+            date_fin_sifca,
+            prog,
+            prog_meq,
+            descr_prog,
+            type_diplome,
+            type_activ,
+            type_parcours,
+            descr_type_parcours,
+            service_enseign,
+            descr_service_enseign,
+            motif_depart,
+            descr_motif_dep,
+            raison_depart,
+            desc_raison_depart
+    )
+select
+    centre.descr as nom_centre,
+    client,
+    population,
+    annee,
+    freq,
+    age_30_juin,
+    age_30_septembre,
+    org,
+    agg.eco_cen,
+    bat,
+    org_hor,
+    descr_org_hor,
+    date_deb,
+    date_fin_sifca,
+    prog,
+    prog_meq,
+    descr_prog,
+    type_diplome,
+    type_activ,
+    type_parcours,
+    descr_type_parcours,
+    service_enseign,
+    descr_service_enseign,
+    motif_depart,
+    descr_motif_dep,
+    raison_depart,
+    desc_raison_depart,
+    total_ele
+from agg
+inner join
+    {{ ref("i_t_ecocen_adultes") }} as centre
+    on agg.eco_cen = centre.eco_cen
