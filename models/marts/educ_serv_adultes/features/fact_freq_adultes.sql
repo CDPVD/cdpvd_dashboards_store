eb94990bc7ab5a97d739f9649260e331837ee5dd
Adama Fall <fallada@cs-soreltracy.qc.ca>
2025-01-28 14:39:45 -0500

    Mise à jour des requêtes SQL pour les rapports d'effectifs des adultes, simplification des colonnes et ajout de nouvelles logiques de calcul d'âge.

--- models/marts/educ_serv_adultes/features/fact_freq_adultes.sql
+++ models/marts/educ_serv_adultes/features/fact_freq_adultes.sql
@@ -24,89 +24,149 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
         ]
     )
 }}
+with 
+  facsources as (
+    select
+      pop.code_perm,
+      pop.fiche,
+      freq.client,
+      pop.population,
+      pop.annee,
+      pop.freq,
+      concat('(',freq.eco_cen,') - ',centre.descr) as [Nom du centre],
+      floor(
+          datediff(
+              day,
+              convert(date, el.date_naissance, 112),
+              cast(concat(pop.annee, '0630') as date)
+          )
+          / 365.25
+      ) as age_30_juin,
+      /* format 112 = yyyymmdd */
+      floor(
+          datediff(
+              day,
+              convert(date, el.date_naissance, 112),
+              cast(concat(pop.annee, '0930') as date)
+          )
+          / 365.25
+      ) as age_30_septembre,
+      /* format 112 = yyyymmdd */
+      freq.org,
+      freq.eco_cen,
+      freq.bat,
+      freq.org_hor,
+      org.descr as descr_org_hor,
+      freq.date_deb,
+      freq.date_fin_sifca,
+      freq.ActivForm,
+      freq.CondAdmiss,
+      wlcon.cf_descr as descr_condadmiss,
+      case
+          when freq.date_fin_sifca is null or freq.date_fin_sifca = '' then 'En cours'
+          else 'Terminé'
+      END as etat_formation,
+      freq.prog,
+      prog.descr_prog,
+      prog.type_diplome,
+      freq.raison_grat_scol,
+      wlgratscol.cf_descr as descr_raison_grat_scol,
+      freq.type_parcours,
+      wlt.cf_descr as descr_type_parcours,
+      freq.service_enseign,
+      wld.cf_descr as descr_service_enseign,
+      freq.motif_depart,
+      mot.cf_descr as descr_motif_dep,
+      freq.raison_depart,
+      wl.cf_descr desc_raison_depart
+  from {{ ref("stg_populations_adultes") }} as pop
+  join {{ ref("dim_eleve_adultes") }} as el on el.code_perm = pop.code_perm
+  join
+      {{ ref("i_e_freq_adultes") }} as freq
+      on freq.fiche = pop.fiche
+      and freq.annee = pop.annee
+      and freq.freq = pop.freq
+  left join {{ ref("i_t_prog_adultes") }} prog on prog.prog = freq.prog
+  left join
+      {{ ref("i_t_wl_descr_adultes") }} wld
+      on wld.code = freq.service_enseign
+      and wld.nom_table = 'X_ServiceEnseign'
+  left join {{ ref("i_t_wl_descr_adultes") }} mot on mot.code = freq.motif_depart 
+      and mot.nom_table = 'X_MotifDep'
+  left join
+      {{ ref("i_t_wl_descr_adultes") }} wl
+      on wl.code = freq.raison_depart
+      and wl.nom_table = 'X_RaisonDepart'
+  left join
+      {{ ref("i_t_wl_descr_adultes") }} wlt
+      on wlt.code = freq.type_parcours
+      and wlt.nom_table = 'X_TypeParcours'
+  left join
+      {{ ref
+  ("i_t_wl_descr_adultes") }} wlcon
+      on wlcon.code = freq.CondAdmiss
+      and wlcon.nom_table = 'X_CondAdmiss'
+  left join
+      {{ ref("i_t_wl_descr_adultes") }} wlgratscol
+      on wlgratscol.code = freq.raison_grat_scol
+      and wlgratscol.nom_table = 'X_RaisonGratScol'
+  left join
+      {{ ref("i_e_o_orghor_adultes") }} org
+      on org.eco_cen = freq.eco_cen
+      and org.org_hor = freq.org_hor
+  inner join
+  {{ ref("i_t_ecocen_adultes") }} as centre
+  on freq.eco_cen = centre.eco_cen
+  where pop.annee >=2020
+  )
 select
-    pop.code_perm,
-    pop.fiche,
-    freq.client,
-    pop.population,
-    pop.annee,
-    pop.freq,
-    floor(
-        datediff(
-            day,
-            convert(date, el.date_naissance, 112),
-            cast(concat(pop.annee, '0630') as date)
-        )
-        / 365.25
-    ) as age_30_juin,
-    /* format 112 = yyyymmdd */
-    floor(
-        datediff(
-            day,
-            convert(date, el.date_naissance, 112),
-            cast(concat(pop.annee, '0930') as date)
-        )
-        / 365.25
-    ) as age_30_septembre,
-    /* format 112 = yyyymmdd */
-    freq.org,
-    freq.eco_cen,
-    freq.bat,
-    freq.org_hor,
-    org.descr as descr_org_hor,
-    freq.date_deb,
-    freq.date_fin_sifca,
-    freq.ActivForm,
-    freq.Cohorte,
-    freq.DonPers,
-    freq.CondAdmiss,
-    wlcon.cf_descr as descr_condadmiss,
-    case
-        when freq.date_fin_sifca is null or freq.date_fin_sifca = '' then 'En cours'
-        else 'Terminé'
-    END as etat_formation,
-    freq.prog,
-    prog.prog_meq,
-    prog.descr_prog,
-    prog.type_diplome,
-    freq.type_activ,
-    freq.type_parcours,
-    wlt.cf_descr as descr_type_parcours,
-    freq.service_enseign,
-    wld.cf_descr as descr_service_enseign,
-    freq.motif_depart,
-    mot.cf_descr as descr_motif_dep,
-    freq.raison_depart,
-    wl.cf_descr desc_raison_depart
-from {{ ref("stg_populations_adultes") }} as pop
-join {{ ref("dim_eleve_adultes") }} as el on el.code_perm = pop.code_perm
-join
-    {{ ref("i_e_freq_adultes") }} as freq
-    on freq.fiche = pop.fiche
-    and freq.annee = pop.annee
-    and freq.freq = pop.freq
-left join {{ ref("i_t_prog_adultes") }} prog on prog.prog = freq.prog
-left join
-    {{ ref("i_t_wl_descr_adultes") }} wld
-    on wld.code = freq.service_enseign
-    and wld.nom_table = 'X_ServiceEnseign'
-left join {{ ref("i_t_wl_descr_adultes") }} mot on mot.code = freq.motif_depart 
-    and mot.nom_table = 'X_MotifDep'
-left join
-    {{ ref("i_t_wl_descr_adultes") }} wl
-    on wl.code = freq.raison_depart
-    and wl.nom_table = 'X_RaisonDepart'
-left join
-    {{ ref("i_t_wl_descr_adultes") }} wlt
-    on wlt.code = freq.type_parcours
-    and wlt.nom_table = 'X_TypeParcours'
-left join
-    {{ ref
-("i_t_wl_descr_adultes") }} wlcon
-    on wlcon.code = freq.CondAdmiss
-    and wlcon.nom_table = 'X_CondAdmiss'
-left join
-    {{ ref("i_e_o_orghor_adultes") }} org
-    on org.eco_cen = freq.eco_cen
-    and org.org_hor = freq.org_hor
-where pop.annee >=2020
+  code_perm,
+  fiche,
+  client,
+  population,
+  annee,
+  freq,
+  [Nom du centre],
+  case
+    when age_30_juin < 16 then '- de 16 ans'
+    when age_30_juin BETWEEN 16 and 17 then '16-17 ans'
+    when age_30_juin BETWEEN 18 and 21 then '18-21 ans'
+    when age_30_juin BETWEEN 22 and 25 then '22-25 ans'
+    when age_30_juin >= 26 then '+ de 26 ans'
+      else null
+  END as interv_age,
+  case
+    when age_30_septembre < 16 then '- de 16 ans'
+    when age_30_septembre BETWEEN 16 and 17 then '16-17 ans'
+    when age_30_septembre BETWEEN 18 and 21 then '18-21 ans'
+    when age_30_septembre BETWEEN 22 and 25 then '22-25 ans'
+    when age_30_septembre >= 26 then '+ de 26 ans'
+      else null
+  END as interv_age_fp,
+  age_30_juin,
+  age_30_septembre,
+  org,
+  eco_cen,
+  bat,
+  org_hor,
+  descr_org_hor,
+  date_deb,
+  date_fin_sifca,
+  ActivForm,
+  CondAdmiss,
+  descr_condadmiss,
+  etat_formation,
+  prog,
+  descr_prog,
+  type_diplome,
+  raison_grat_scol,
+  descr_raison_grat_scol,
+  type_parcours,
+  descr_type_parcours,
+  service_enseign,
+  descr_service_enseign,
+  motif_depart,
+  descr_motif_dep,
+  raison_depart,
+  desc_raison_depart
+from facsources
