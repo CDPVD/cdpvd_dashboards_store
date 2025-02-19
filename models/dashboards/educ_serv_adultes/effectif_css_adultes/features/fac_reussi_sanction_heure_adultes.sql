d93b0b9096f6125b63145e1d13e7fbac332e2818
Adama Fall <fallada@cs-soreltracy.qc.ca>
2025-02-19 18:16:27 -0500

    features: santion reussite

---
+++ models/dashboards/educ_serv_adultes/effectif_css_adultes/features/fac_reussi_sanction_heure_adultes.sql
@@ -0,0 +1,92 @@
+{#
+CDPVD Dashboards store
+Copyright (C) 2024 CDPVD.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#}
+
+with cte as
+  (
+    SELECT fac.code_perm
+      , fac.fiche
+      , fac.annee
+      , fac.freq
+      , mat
+      , grp
+      , noseqmat
+      , indmatetei
+      , grh
+      , disc
+      , ordchrono
+      , nbhresprev
+      , nbminrea
+      , facr.date_fin
+      , statutprofil
+      , resens
+      , facr.date_deb
+      , annee_sanct
+      , mois_sanct
+      , jour_sanct
+      , CAST(CONCAT(annee_sanct, '-', 
+                RIGHT(CONCAT('0', mois_sanct), 2), '-', 
+                RIGHT(CONCAT('0', jour_sanct), 2)) AS DATE) AS date_sanct
+      , indtransm
+      , service
+      , case
+        when TRY_CAST(res AS INT) >= 60 or res = 'SU' then 'SU'
+        when TRY_CAST(res AS INT) < 60 then 'EC'
+        else res
+       END as sanc
+      , res
+      , nbhresstage
+      , descrmat
+      ,fac.eco_cen
+      ,fac.client,
+      fac.population,
+      fac.interv_age,
+      fac.interv_age_fp,
+      fac.org_hor,
+      fac.descr_org_hor,
+      fac.activform,
+      fac.condadmiss,
+      fac.descr_condadmiss,
+      fac.etat_formation,
+      fac.prog,
+      fac.descr_prog,
+      fac.type_diplome,
+      fac.raison_grat_scol,
+      fac.descr_raison_grat_scol,
+      fac.type_parcours,
+      fac.descr_type_parcours,
+      fac.service_enseign,
+      fac.descr_service_enseign,
+      fac.motif_depart,
+      fac.descr_motif_dep,
+      fac.raison_depart,
+      fac.desc_raison_depart,
+      el.genre,
+      concat('(',facr.fiche,') ',el.prenom, ' ', el.nom) as prenom_nom,
+      el.lang_matern,
+      el.desc_lang_matern
+    FROM {{ ref("fact_reussite_adultes") }} facr
+    inner join {{ ref("fact_freq_adultes") }} as fac
+      on fac.code_perm = facr.code_perm
+      and fac.fiche = facr.fiche
+      and fac.annee = facr.annee
+      and fac.freq = facr.freq
+    inner join {{ ref("dim_eleve_adultes") }} as el on el.code_perm = facr.code_perm
+    where res !=''
+  )
+select *
+from cte
