2f88253abfe4f835d2b34d7ca462fdf5075a9018
Adama Fall <fallada@cs-soreltracy.qc.ca>
2025-06-25 10:44:03 -0400

    init dagster

--- projet_cssst/projet_cssst/assets.py
+++ projet_cssst/projet_cssst/assets.py
@@ -1,17 +1,7 @@
-from dagster import asset, AssetExecutionContext
-import subprocess
 
-@asset
-def test_dbt_cli_debug(context: AssetExecutionContext):
-   result = subprocess.run(
-       ["poetry", "run", "dbt", "build", "--select", "i_gpm_e_dan"],
-       cwd="/home/cssst/cdpvd_dashboards_store",
-       capture_output=True,
-       text=True
-   )
-   context.log.info("--- STDOUT ---")
-   context.log.info(result.stdout)
-   context.log.info("--- STDERR ---")
-   context.log.info(result.stderr)
-   if result.returncode != 0:
-       raise Exception("dbt debug failed. See logs above.")
+from pathlib import Path
+from dagster_dbt import load_assets_from_dbt_manifest
+
+dbt_models = load_assets_from_dbt_manifest(
+    manifest=Path("/home/cssst/cssst.dashboards_store/target/manifest.json")
+)
--- projet_cssst/projet_cssst/definitions.py
+++ projet_cssst/projet_cssst/definitions.py
@@ -1,6 +1,21 @@
-from dagster import Definitions
+from dagster import Definitions, define_asset_job
+from dagster_dbt import DbtCliResource
+from .assets import dbt_models
 
-from .assets import test_dbt_cli_debug
+# Configuration de la ressource dbt CLI
+dbt_resource = DbtCliResource(
+    project_dir="/home/cssst/cssst.dashboards_store",
+    profiles_dir="/home/cssst/.dbt",
+    profile="cssst_dashboards_store",
+    executable="/home/cssst/.cache/pypoetry/virtualenvs/cdpvd-dashboards-store-PwHv-ypr-py3.10/bin/dbt",
+)
+
+# Job principal : exécute tous les assets
+build_all_job = define_asset_job("build_all")
+
+# Définition finale du dépôt Dagster
 defs = Definitions(
-   assets=[test_dbt_cli_debug]
+    assets=[dbt_models],
+    resources={"dbt": dbt_resource},
+    jobs=[build_all_job],
 )
