0551a6dbbf0718bd9bda623806a73f171a8362ab
sadqim <sadqim@csvdc.qc.ca>
2025-09-30 15:07:56 -0400

    fix : modifier la table utilisateur

--- models/rls/utilisateurs_gestionnaires.sql
+++ models/rls/utilisateurs_gestionnaires.sql
@@ -12,6 +12,27 @@ with
         from {{ ref("i_identite") }}
         where
             corps_emploi  like '1%'
+    ), eco_princ as (
+        select
+            cle_organisationnelle,
+            compte_authentification,
+            nom,
+            prenom,
+            CASE
+                WHEN ecole_principale IS NULL THEN ecoles
+                WHEN ecoles IS NULL OR LTRIM(RTRIM(ecoles)) = '' THEN ecole_principale
+                WHEN NOT EXISTS (
+                    SELECT 1
+                    FROM STRING_SPLIT(ecoles, ',') AS s
+                    WHERE LTRIM(RTRIM(s.value)) = LTRIM(RTRIM(ecole_principale))
+                )
+                THEN CONCAT(ecoles, ',', ecole_principale)
+                ELSE ecoles
+            END AS ecoles,
+            corps_emploi,
+            ecole_principale,
+            description_corps_emploi_principal
+        from users
     )
 select
     cle_organisationnelle,
@@ -23,4 +44,4 @@ select
     ecole_principale,
     description_corps_emploi_principal,
     value as ecole
-from users cross apply string_split(ecoles, ',')
+from eco_princ cross apply string_split(ecoles, ',')
