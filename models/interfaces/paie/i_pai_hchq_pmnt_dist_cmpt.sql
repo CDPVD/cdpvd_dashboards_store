c05192106deb28114bbe3788afa48acd2108e94f
alluardj <alluardj@csvdc.qc.ca>
2025-10-17 08:47:43 -0400

    feat(interface): add pai_hchq_pmnt_dist_cmpt

---
+++ models/interfaces/paie/i_pai_hchq_pmnt_dist_cmpt.sql
@@ -0,0 +1,24 @@
+{#
+CDPVD Dashboards store
+Copyright (C) 2024 CDPVD.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#}
+select
+    matr
+    , date_cheq
+    , no_seq
+    , no_cmpt
+    , mnt_dist
+from {{ var("database_paie") }}.dbo.pai_hchq_pmnt_dist_cmpt
