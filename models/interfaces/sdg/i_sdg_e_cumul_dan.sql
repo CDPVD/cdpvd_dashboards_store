b08e1ecd1b37ab7bf2bef545d0dff40088d42992
alluardj <alluardj@csvdc.qc.ca>
2025-10-29 08:45:07 -0400

    feat: add interfaces i_sdg_e_dan, i_sdg_e_cumul_dan + schema

---
+++ models/interfaces/sdg/i_sdg_e_cumul_dan.sql
@@ -0,0 +1,24 @@
+{#
+CDPVD Dashboards store
+Copyright (C) 2024 CDPVD.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#}
+select
+    fiche,
+    id_sdg,
+    annee,
+    sum_solde_fac_cour,
+    trop_percu_an_cour
+from {{ var("database_sdg") }}.dbo.sdg_e_cumul_dan
