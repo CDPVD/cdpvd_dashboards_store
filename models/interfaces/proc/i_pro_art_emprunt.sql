80c0524c908e6b2a9c76bfc04aa9899acf7eef90
alluardj <alluardj@csvdc.qc.ca>
2025-10-29 08:50:19 -0400

    fix: change i_pro_art_emp

--- models/interfaces/proc/i_pro_art_emprunt.sql
+++ models/interfaces/proc/i_pro_art_emprunt.sql
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #}
 select
-    concat(code_emprunt, '_CRIF') as code_emprunt,
+    code_emprunt,
     ecocen as eco_cen,
     annee,
     cout_ttc,
