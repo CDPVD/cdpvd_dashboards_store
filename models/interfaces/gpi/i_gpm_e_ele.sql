34a9f9598a9bba21169d468047f1cb4dea007934
alluardj <alluardj@csvdc.qc.ca>
2025-10-30 13:35:08 -0400

    feat(fact_contact): add contact information

---
+++ models/interfaces/gpi/i_gpm_e_ele.sql
@@ -0,0 +1,34 @@
+{#
+CDPVD Dashboards store
+Copyright (C) 2024 CDPVD.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#}
+select 
+    fiche, 
+    nom, 
+    pnom, 
+    code_perm, 
+    sexe, 
+    date_naiss as date_naissance,
+    nom_pere, 
+    pnom_pere,
+    nom_mere, 
+    pnom_mere,
+    nom_tuteur, 
+    pnom_tuteur,
+    adr_electr_pere, 
+    adr_electr_mere,
+    adr_electr_tuteur
+from {{ var("database_gpi") }}.dbo.gpm_e_ele
