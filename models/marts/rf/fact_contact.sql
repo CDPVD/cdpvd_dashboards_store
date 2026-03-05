695bdec1436899acac48cbafc15d4fe8e3afc05c
alluardj <alluardj@csvdc.qc.ca>
2026-03-05 09:51:03 -0500

    ix(fact_contact): add SDG contact and keep only the primary address per code_perm and fiche

--- models/marts/rf/fact_contact.sql
+++ models/marts/rf/fact_contact.sql
@@ -19,13 +19,48 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 with 
 	-- Recuperer l'ensemble des adresses FGJ-FP-FGA
 	adr as (
-		-- FGJ (priorité 2)
+		-- Avt-Garde
+        select
+			ele.code_perm,
+			ele.fiche,
+			ele.nom, 
+    		ele.prenom, 
+			ele.nom_mere, 
+			ele.pnom_mere,
+			null as adr_electr_mere,
+			ele.nom_pere, 
+			ele.pnom_pere,
+			null as adr_electr_pere, 
+			ele.nom_tuteur, 
+			ele.pnom_tuteur,
+			null as adr_electr_tuteur,
+			adr.type_contact as type_adr,
+			ele.date_maj,
+			ltrim(rtrim(
+				isnull(nullif(adr.app, '') + '-', '') +
+				isnull(nullif(adr.no_civ, '') + ' ', '') +
+				isnull(nullif(adr.orient_rue, '') + ' ', '') +
+				isnull(nullif(adr.genre_rue, '') + ' ', '') +
+				isnull(nullif(adr.rue, '') + ', ', '') +
+				isnull(nullif(adr.ville, '') + ', ', '') +
+				isnull(nullif(adr.code_post, ''), '')
+			)) as adresse
+		from {{ ref("contact_sdg") }} ele
+		left join {{ ref("i_sdg_e_contact") }} adr
+			on adr.fiche = ele.fiche
+		where 
+			ele.code_perm is not null
+			and adr.adr_eleve = '1' 					-- adresse principal uniquement
+		
+		union all
+
+		-- FGJ
         select
 			ele.code_perm,
 			right('0000000' + cast(adr.fiche as varchar(7)), 7) as fiche,
 			ele.nom, 
-    		ele.prenom, 
-    		ele.nom_mere, 
+    		ele.prenom,
+			ele.nom_mere, 
 			ele.pnom_mere,
 			ele.adr_electr_mere,
 			ele.nom_pere, 
@@ -35,8 +70,7 @@ with
 			ele.pnom_tuteur,
 			ele.adr_electr_tuteur,
 			adr.type_adr,
-			adr.date_effect,
-			adr.date_fin,
+			ele.date_maj,
 			ltrim(rtrim(
 				isnull(nullif(adr.app, '') + '-', '') +
 				isnull(nullif(adr.no_civ, '') + ' ', '') +
@@ -45,18 +79,16 @@ with
 				isnull(nullif(adr.rue, '') + ', ', '') +
 				isnull(nullif(adr.ville, '') + ', ', '') +
 				isnull(nullif(adr.code_post, ''), '')
-			)) as adresse,
-        	2 as priorite
-		from {{ ref("i_e_adr") }} adr
-		left join {{ ref("i_e_ele") }} ele
-			on ele.fiche = adr.fiche
+			)) as adresse
+		from {{ ref("i_e_ele") }} ele
+		left join {{ ref("i_e_adr") }} adr
+			on adr.fiche = ele.fiche
 		where 
-			cast(getdate() as date) >= adr.date_effect 	-- l'adresse doit etre effective aujourd'hui
-			and ele.code_perm is not null
-
+			ele.code_perm is not null
+			and adr.ind_envoi_meq = '1' 				-- adresse principal uniquement
 		union all
 
-		-- FP et FGA (priorité 1)
+		-- FP et FGA
 		select
 			ele.code_perm,
 			case
@@ -65,7 +97,7 @@ with
 			end as fiche,
 			ele.nom, 
     		ele.prenom,
-    		ele.nom_mere, 
+			ele.nom_mere, 
 			ele.pnom_mere,
 			ele.adr_electr_mere,
 			ele.nom_pere, 
@@ -75,8 +107,7 @@ with
 			ele.pnom_tuteur,
 			ele.adr_electr_tuteur,
 			adr.typeadr as type_adr,
-			adr.date_effect,
-			adr.date_fin,
+			ele.date_maj,
 			ltrim(rtrim(
 				isnull(adr.app + '-', '') +
 				isnull(adr.no_civ + ' ', '') +
@@ -85,159 +116,48 @@ with
 				isnull(adr.rue + ', ', '') +
 				isnull(adr.ville + ', ', '') +
 				isnull(adr.code_post, '')
-			)) as adresse,
-        	1 as priorite
-		from {{ ref("i_e_adr_adultes") }} adr
-		left join {{ ref("i_e_ele_adultes") }} ele
-			on ele.fiche = adr.fiche
-	where 
-		cast(getdate() as date) >= adr.date_effect 	-- l'adresse doit etre effective aujourd'hui
-		and ele.code_perm is not null
+			)) as adresse
+		from {{ ref("i_e_ele_adultes") }} ele
+		left join {{ ref("i_e_adr_adultes") }} adr
+			on adr.fiche = ele.fiche
+		where 
+			ele.code_perm is not null
+			and adr.envoimeq = '1' 					 -- adresse principal uniquement
 	
-	-- Ajout d'un seq_id pour conserver les dernieres lignes par type d'adresse
-	), seq as (
-        select 
-			*,
-			row_number() over (partition by code_perm, type_adr order by date_effect desc) as seq_id
-        from adr
-
-	-- Conserver les dernieres adresses par type
+	-- Pour chaque cp, conserver la derniere maj et la fiche associée
 	), latest as (
 		select *
-		from seq
+		from (
+			select 
+				*,
+				row_number() over (partition by code_perm order by date_maj desc) as seq_id
+			from adr
+		) as seq
 		where seq_id = 1
 
-	-- Priorité inter-secteur FP-FGA > FGJ. Si l’élève existe en FP-FGA, on ignore complètement FGJ pour lui.
-	), prioritized as (
-    select *
-    from (
-        select 
-			*,
-            row_number() over (
-                partition by code_perm
-                order by priorite asc
-            ) as rn_prior
-        from latest
-    ) as temp
-    where rn_prior = 1
-
-	-- Redefinir les types d'adresses en role
-	), roles as (
-		select 
-			fiche, 
-			code_perm,
-			nom, 
-    		prenom, 
-			'mere' as role, 
-			date_effect, 
-			date_fin,
-			adresse, 
-    		nom_mere, 
-			pnom_mere,
-			adr_electr_mere,
-			nom_pere, 
-			pnom_pere,
-			adr_electr_pere, 
-			nom_tuteur, 
-			pnom_tuteur,
-			adr_electr_tuteur
-		from prioritized
-		where type_adr in ('1','3')
-
-		union all
-
-		select 
-			fiche, 
-			code_perm, 
-			nom, 
-    		prenom, 
-			'pere' as role, 
-			date_effect, 
-			date_fin, 
-			adresse, 
-    		nom_mere, 
-			pnom_mere,
-			adr_electr_mere,
-			nom_pere, 
-			pnom_pere,
-			adr_electr_pere, 
-			nom_tuteur, 
-			pnom_tuteur,
-			adr_electr_tuteur
-		from prioritized
-		where type_adr in ('1','2')
-
-		union all
-
-		select 
-			fiche, 
-			code_perm, 
-			nom, 
-    		prenom, 
-			'tuteur' as role, 
-			date_effect, 
-			date_fin, 
-			adresse, 
-    		nom_mere, 
-			pnom_mere,
-			adr_electr_mere,
-			nom_pere, 
-			pnom_pere,
-			adr_electr_pere, 
-			nom_tuteur, 
-			pnom_tuteur,
-			adr_electr_tuteur
-		from prioritized
-		where type_adr = '4'
-	
-		union all
-
-		select 
-			fiche, 
-			code_perm, 
-			nom, 
-    		prenom, 
-			'eleve' as role, 
-			date_effect, 
-			date_fin, 
-			adresse, 
-    		nom_mere, 
-			pnom_mere,
-			adr_electr_mere,
-			nom_pere, 
-			pnom_pere,
-			adr_electr_pere, 
-			nom_tuteur, 
-			pnom_tuteur,
-			adr_electr_tuteur
-		from prioritized
-		where type_adr = 5
-
-	-- ajout d'un nouveau seq_id par role
-	), seq2 as (
-    select 
-		*,
-        row_number() over (partition by code_perm, role order by date_effect desc) as seq_id
-    from roles
-)
+	)
 
 select
     code_perm,
-	nom, 
-    prenom, 
-	fiche,
-	max(case when role = 'eleve' and seq_id = 1 then adresse end) as adresse_eleve,
-    nom_mere, 
+    nom,		 
+    prenom,
+    fiche,
+	nom_mere, 
 	pnom_mere,
 	adr_electr_mere,
-    max(case when role = 'mere' and seq_id = 1 then adresse end) as adresse_mere,
 	nom_pere, 
 	pnom_pere,
 	adr_electr_pere, 
-    max(case when role = 'pere' and seq_id = 1 then adresse end) as adresse_pere,
 	nom_tuteur, 
 	pnom_tuteur,
 	adr_electr_tuteur,
-    max(case when role = 'tuteur' and seq_id = 1 then adresse end) as adresse_tuteur
-from seq2
-group by code_perm, nom, prenom, fiche, nom_mere, pnom_mere, adr_electr_mere, nom_pere, pnom_pere, adr_electr_pere, nom_tuteur, pnom_tuteur, adr_electr_tuteur
+	adresse,
+	case 
+		when type_adr = '1' then 'Père et mère'
+		when type_adr = '2' then 'Père'
+		when type_adr = '3' then 'Mère'
+		when type_adr = '4' then 'Tuteur'
+		when type_adr = '5' then 'Elève'
+		else 'Autre'
+	end as adresse_contact
+from latest
