1720d172371881e8ba1596383598452ddf9d6150
sadqim <sadqim@csvdc.qc.ca>
2026-03-13 10:00:55 -0400

    feat: ajout des catégories d'absence aux tables de reporting

--- models/dashboards/educ_serv/absenteeism/pbi_tables/cdpvd_abstsm_report_filters.sql
+++ models/dashboards/educ_serv/absenteeism/pbi_tables/cdpvd_abstsm_report_filters.sql
@@ -16,48 +16,69 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #}
 {#
-    Gather all the filters into one table to allow for between-pages corss filtering.
+    Table de filtres consolidée.
+
+    Regroupe toutes les dimensions utiles (année, école, étape, type, catégorie,
+    groupe) avec une clé de filtre unique pour permettre le cross-filtering
+    entre pages dans Power BI.
 #}
 {{ config(alias="cdpvd_report_filters") }}
 
+-- ============================================================================
+-- ÉTAPE 1: Extraction des dimensions sources
+-- ============================================================================
 with
     source as (
         select
+            annee_scolaire,
             annee,
             eco,
-            coalesce(etape_friendly, 'Tout') as etape_friendly,
+            coalesce(ordre_enseignement, 'Tout') as ordre_enseignement,
             event_kind,
-            coalesce(groupe, 'Tout') as groupe
+			coalesce(category_abs, 'Tout') as category_abs,
+            groupe
         from {{ ref("cdpvd_abstsm_stg_daily_metrics") }} dly
-        group by annee, cube (eco, groupe, etape_friendly), event_kind
+        group by annee_scolaire,annee, cube (eco, ordre_enseignement, category_abs),groupe, event_kind
     ),
+    -- ========================================================================
+    -- ÉTAPE 2: Enrichissement des libellés d'école
+    -- ========================================================================
     nomeco as (
         select
+            src.annee_scolaire,
+            src.ordre_enseignement,
             src.annee,
             src.eco,
-            groupe,
-            etape_friendly,
-            event_kind,
-            coalesce(school_friendly_name, 'Tout le CSS') as school_friendly_name
+            src.groupe,
+            src.event_kind,
+            src.category_abs,
+            coalesce(eco.school_friendly_name, 'Tout le CSS') as school_friendly_name
         from source src
         left join
             {{ ref("dim_mapper_schools") }} as eco
             on src.eco = eco.eco
-            and src.annee = eco.annee
+            and src.annee = eco.annee        
     )
+-- ============================================================================
+-- ÉTAPE 3: Sélection finale avec génération de la clé de filtre
+-- ============================================================================
+
 select
+    annee_scolaire,
     annee,
     school_friendly_name,
-    etape_friendly,
+    ordre_enseignement,
     event_kind,
+    category_abs,
     groupe,
     {{
         dbt_utils.generate_surrogate_key(
             [
-                "annee",
+                "annee_scolaire",
                 "school_friendly_name",
-                "etape_friendly",
+                "ordre_enseignement",
                 "event_kind",
+                "category_abs",
                 "groupe",
             ]
         )
@@ -65,3 +86,4 @@ select
     -- RLS hooks :
     eco
 from nomeco
+
