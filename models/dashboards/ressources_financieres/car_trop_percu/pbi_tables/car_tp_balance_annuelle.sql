140a43fe2ececb9a04a61f59cbaa14598284418c
alluardj <alluardj@csvdc.qc.ca>
2026-05-12 09:02:34 -0400

    chore: licence

--- models/dashboards/ressources_financieres/car_trop_percu/pbi_tables/car_tp_balance_annuelle.sql
+++ models/dashboards/ressources_financieres/car_trop_percu/pbi_tables/car_tp_balance_annuelle.sql
@@ -1,6 +1,6 @@
 {#
 CDPVD Dashboards store
-Copyright (C) 2025 CDPVD.
+Copyright (C) 2024 CDPVD.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
