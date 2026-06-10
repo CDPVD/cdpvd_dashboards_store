625c74bd25f251028b8e61767526c346a0e88fda
Adama Fall <fallada@cs-soreltracy.qc.ca>
2026-06-10 18:52:30 -0400

    Feat : ajout doc, pbit fga et seed pour le depassement

--- models/dashboards/educ_serv_adultes/portrait_css_fpfga/pbi_tables/sanctions/report_sanction_adultes.sql
+++ models/dashboards/educ_serv_adultes/portrait_css_fpfga/pbi_tables/sanctions/report_sanction_adultes.sql
@@ -22,15 +22,48 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
         ]
     )
 }}
+{%- set source_relation = adapter.get_relation(
+    database=target.database,
+    schema=target.schema + "_dashboard_effectif_css_adultes_seeds",
+    identifier="depassement_heure_mat",
+) -%}
+{% set table_exists = source_relation is not none %}
+{% set depassement_par_defaut = var("dashboards", {}).get("educ_serv_adultes", {}).get("portrait_css_fpfga", {}).get("depassement_par_defaut", "150") %}
+
+{% if table_exists %}
+    {% set table_seed = "{{ ref('depassement_heure_mat') }}" %}
+    {% if execute %}
+        {{
+            log(
+                "La seed 'depassement_heure_mat' existe. Les matieres de la seed auront des valeurs personnalisées sinon la valeur par défaut de " ~ depassement_par_defaut ~ " sera utilisée.",
+                true,
+            )
+        }}
+    {% endif %}
+{% else %}
+    {% if execute %}
+        {{
+            log("La seed 'depassement_heure_mat' n'existe pas. La valeur par défaut de " ~ depassement_par_defaut ~ " sera utilisée pour toutes les matières. Elle est configurable dans les variables du projet.", true)
+        }}
+    {% endif %}
+{% endif %}
+
 with
     cte as (
         select *, round(cast(nbminrea as float) / 60, 2) as nbhresrea
         from {{ ref("fac_reussi_sanction_heure_adultes") }} facr
     ),
     depasse as (
-        select cte.*, coalesce(dep.depassement, 150) as depassement
+        select cte.*,
+        {%- if table_exists %}
+        coalesce(dep.depassement, {{ depassement_par_defaut }}) as depassement
+        {% else -%}
+        {{ depassement_par_defaut }} as depassement
+        {% endif -%}
         from cte
-        left join {{ ref("depassement_heure_mat") }} dep on cte.mat = dep.matieres
+        {%- if table_exists %}
+        left join {{source_relation}} dep on cte.mat = dep.matieres
+        {% endif -%}
     )
 select *, CASE WHEN nbhresrea > depassement THEN nbhresrea - depassement ELSE 0 END as depassement_heure, CASE WHEN nbhresrea > depassement THEN 1 ELSE 0 END as nbre_ele_depasse
 from depasse
